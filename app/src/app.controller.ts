a4536f6a319152853fb6166e5a9b468868117baa
donggni0712 <donggni0712@naver.com>
2023-09-10 13:31:05 +0900

    feat(): Make getIpAndPort method

--- app/src/app.controller.ts
+++ app/src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
 import { AppService } from './app.service';
+import * as os from 'os';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('ip')
+  getIpAndPort(): string {
+    const ip = this.appService.getServerIp();
+    const port = this.appService.getServerPort();
+    return `Server is running on IP: ${ip} and Port: ${port}`;
   }
 }
--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import * as os from 'os';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getServerIp(): string {
+    const interfaces = os.networkInterfaces();
+    var ip: string;
+
+    for (let k in interfaces) {
+      for (let k2 in interfaces[k]) {
+        let address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+          ip = address.address;
+        }
+      }
+    }
+    return ip;
+  }
+
+  getServerPort(): string {
+    const addressInfo = this.httpAdapterHost.httpAdapter
+      .getHttpServer()
+      .address();
+    const port =
+      typeof addressInfo === 'string' ? addressInfo : addressInfo?.port;
+    return port;
   }
 }
