768d8e2b0c29f71cfa4177e9452fce283098ad74
donggni0712 <donggni0712@naver.com>
2023-09-12 00:05:11 +0900

    feat(web): Add logs button

--- web/app.js
+++ web/app.js
@@ -8,3 +8,24 @@ document.getElementById("checkIpButton").addEventListener("click", async () => {
     document.getElementById("result").innerText = "Error fetching data";
   }
 });
+
+document.getElementById("getLogsButton").addEventListener("click", async () => {
+  try {
+    const response = await fetch("http://localhost:3000/logs");
+    const data = await response.json();
+    const logsElement = document.getElementById("logs");
+
+    let logsHTML = "<ul>";
+    data.forEach((log) => {
+      logsHTML += `<li>ID: ${log.id}, IP: ${
+        log.queriedIP
+      }, Timestamp: ${new Date(log.timestamp).toLocaleString()}</li>`;
+    });
+    logsHTML += "</ul>";
+
+    logsElement.innerHTML = logsHTML;
+  } catch (error) {
+    console.error("Error fetching logs:", error);
+    document.getElementById("logs").innerText = "Error fetching logs";
+  }
+});
